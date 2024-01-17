c856d65f7e4efd206c11d8bf181ee40368b9e333
Te0dorTyuli3v <102541717+Te0dorTyuli3v@users.noreply.github.com>
2024-01-17 17:47:34 +0200

    Add files via upload

---
+++ DeliverySchedule.cs
@@ -0,0 +1,11 @@
+﻿namespace Driver__Route__Delivery_ScheduleProject
+{
+	public class DeliverySchedule
+	{
+		public int DeliveryScheduleId { get; set; }
+		public int DriverId { get; set; }
+		public int RouteId { get; set; }
+		public DateTime ScheduleDate { get; set; }
+		public string Status { get; set; }
+	}
+}
